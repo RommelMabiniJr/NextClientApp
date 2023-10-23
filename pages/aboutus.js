002050431fc53b9347f51b80b35e143fa309ec0d
Mark Dave R. Amamangpang <markdaveamamangpang@gmail.com>
2023-10-24 07:03:36 +0800

    Merge pull request #1 from Mamarkyy/main Updated Landing page

---
+++ pages/aboutus.js
@@ -0,0 +1,21 @@
+import Head from "next/head";
+
+const AboutUs = () => {
+  return (
+    <div className="">
+      <div>
+        <span></span>
+        <h2 className="ml-7">About Us</h2>
+      </div>
+      <divider />
+      <div>
+        <p>
+          We are a team of developers passionate about creating amazing
+          software.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutUs;
