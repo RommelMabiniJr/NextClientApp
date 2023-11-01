4a06a0c6e4d2dc86295760b1e8f27a6da347dda9
Mark Dave R. Amamangpang <markdaveamamangpang@gmail.com>
2023-11-01 13:02:10 +0800

    Landing page updated, there were unfortunate errors was stuck (I encounter error, Build failure due to webpack errors. pero was able to fux it there was missing node modules lang pala, i accidentally delete it hahaha)

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,24 @@
-import 'primereact/resources/themes/lara-light-indigo/theme.css'; // this is active theme
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
-import '../styles/layout/layout.scss';
-import '../styles/adjustments.css';
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // this is active theme
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
+import "../styles/layout/layout.scss";
+import "../styles/adjustments.css";
+import "../styles/globals.css";
 
-import '../layout/demo/TimelineDemo.css' // >>> remove after testing
+import "../layout/demo/TimelineDemo.css"; // >>> remove after testing
 
-import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from '../store/reducers/rootReducer';
-import { SessionProvider } from 'next-auth/react';
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../store/reducers/rootReducer";
+import { SessionProvider } from "next-auth/react";
 
 function MyApp({ Component, pageProps }) {
   return (
     <SessionProvider session={pageProps.session}>
-        <Component {...pageProps} />
+      <Component {...pageProps} />
     </SessionProvider>
   );
 }
 
 export default MyApp;
-
